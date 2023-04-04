82ad36e5acf5569a6a6968536e6696a15f6f6fb7
Kiersten White <kierstenswhite@gmail.com>
2023-04-04 09:35:25 -0400

    Basic Chat Feature that updates from the bottom and can delete. No formatting, but does have borders around each section

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,6 +1,21 @@
+import { ChatForm } from "./chatForm.js";
+import { Messages } from "./chat.js";
+
 
 export const Nutshell = () => {
-
+      return `
+      
+      <article id="chatMessages">
+            <section id="chatForm">
+                  <h2>Send a message!</h2>
+                  ${ ChatForm()}
+            </section>
+            <section id="newMessages">
+                  <h2>New Messages</h2>
+                  ${Messages()}
+            </section>
+      </article>
+      `
   
       // Render all your UI components here
 }
---
+++ src/scripts/chatForm.js
@@ -0,0 +1,43 @@
+// Created by Kiersten White
+
+import { sendMessage } from "./dataAccessChat.js" //needs to change to dataAccess.js before being pushed up
+
+// Creates data for the Chat Form area, including some HTML styling using Bootstrap
+export const ChatForm = () => {
+    let html = `
+        <div class="mb-3">
+            <label for="username" class="form-label">Username:</label>
+            <input type="text" name="username" class="form-control" placeholder="purplemonkey34">
+        </div>
+        <div class="mb-3">
+            <label for="chatMessage" class="form-label">Type your message here:</label>
+            <input type="text" name="chatMessage" class="form-control" placeholder="What r u doing 2nite xD">
+        </div>
+        <button class="button" id="submitMessage">Send Message</button>
+    `
+    return html
+}
+
+// dashboard variable targets the <article id="dashboard"> in index.html
+const dashboard = document.querySelector("#dashboard")
+
+// Listens for the Submit Message button to be clicked, takes the data (in this case username and each message), and sends that data to be stored in the API
+dashboard.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "submitMessage") {
+        
+        const username = document.querySelector("input[name='username']").value
+        const chatMessage = document.querySelector("input[name='chatMessage']").value
+
+        const dataToSendToAPI = {
+            username: username,
+            chatMessage: chatMessage,
+        }
+
+        sendMessage(dataToSendToAPI)
+
+    }
+})
+
+
+// Preproposal on 4/14 - one paragraph (3-4 sentences) - Slack them to all instructors(Sarah, Tommy, Jordan, Barry)
+// Character Tracker with randomizers?
